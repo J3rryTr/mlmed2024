c0b481b515057275c79cba4f052e1e6b69c2dd66
unknown <trungtran20802@gmail.com>
2024-03-26 11:27:20 +0700

    update

--- lab1/run.py
+++ lab1/run.py
@@ -1,8 +1,30 @@
-import train
-import dataset
-import model
-
+import pandas as pd
 import torch
+import torch.nn as nn
+from utils.data_loading import ECG
+from torch.utils.data import DataLoader
+from train import train_test
+from CNN.cnn_custom import CNN
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-train.Training(model=model, train_loader=dataset.train_loader, test_loader=dataset.test_loader, epochs=3, criterion=model.criterion, optimizer=model.optimizer, device=device, save_path='/home/jerry/code/FaceDetection/output').train()
+train = pd.read_csv('/home/jerry/code/MLmedicine/lab1/dataset/mitbih_train.csv', header=None)
+test = pd.read_csv('/home/jerry/code/MLmedicine/lab1/dataset/mitbih_test.csv', header=None)
+
+X_train, y_train = train.iloc[:, :187], train[187]
+X_test, y_test = test.iloc[:, :187], test[187]
+
+train_loader = DataLoader(ECG(X=X_train, y=y_train), batch_size=32, shuffle=True)
+test_loader = DataLoader(ECG(X=X_test, y=y_test), batch_size=32, shuffle=False)
+
+model = CNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+
+train_test(
+    model=model,
+    train_loader=train_loader, test_loader=test_loader,
+    epochs=100, criterion=criterion,
+    optimizer=optimizer, device=device
+).runloop()
